59785fbd7e8599d6f5b6cb01ca9d0514190a6c50
AngelinxX <fajardo_aa@outlook.com>
2023-09-19 23:10:51 -0500

    Cambios BDD

--- Scripts/creacion_cuenta.sql
+++ Scripts/creacion_cuenta.sql
@@ -2,8 +2,8 @@
 drop table cuenta
 
 create table cuenta(
-	numero_cuenta char(10) not null,
-	cedula_propietario char(10) not null,
+	numero_cuenta char(5) not null,
+	cedula_propietario char(5) not null,
 	fecha_creacion date not null,
 	saldo money not null,
 	constraint cuenta_pk primary key (numero_cuenta)
--- Scripts/creacion_videojuegos.sql
+++ Scripts/creacion_videojuegos.sql
@@ -4,7 +4,7 @@ drop table videojuegos
 create table videojuegos(
 	codigo int not null,
 	nombre varchar (100) not null,
-	descripcion varchar (300) not null,
+	descripcion varchar (300),
 	valoracion int not null,
 	constraint videojuegos_pk primary key (codigo)
 )
