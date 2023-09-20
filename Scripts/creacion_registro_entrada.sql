6a10b2808c78dcdb215bbde25961c05a2285189d
AngelinxX <fajardo_aa@outlook.com>
2023-09-19 22:17:41 -0500

    correccion bdd'

--- Scripts/creacion_registro_entrada.sql
+++ Scripts/creacion_registro_entrada.sql
@@ -4,7 +4,7 @@ drop table registro_entrada
 create table registro_entrada(
 	codigo_registro int not null,
 	cedula_empleado char (10) not null,
-	fecha date,
-	hota time,
+	fecha date not null,
+	hota time not null,
 	constraint registro_entrada_pk primary key (codigo_registro)
 )
